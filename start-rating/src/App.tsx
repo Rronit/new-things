4fe89a13e8cc82008eecf06fde1f71b0e8698965
Rronit <rronit2015@gmail.com>
2024-12-03 21:58:28 +0530

    feat: avg rating showing

--- start-rating/src/App.tsx
+++ start-rating/src/App.tsx
@@ -3,6 +3,16 @@ import "./App.css";
 
 export const App: React.FC = () => {
   const [starCount, setStarCount] = useState<number>(0);
+  const [avgRating, setAvgRating] = useState<number>(3.3);
+  const parts = avgRating.toString().split(".");
+  // const yellowWidth = 35;
+  function getYellowWidth(index: number) {
+    const integerPart = parseInt(parts[0], 10); // Integer part (3)
+    const fractionalPart = parseInt(parts[1], 10);
+    if (index < integerPart) return 100;
+    else if (index === integerPart) return fractionalPart * 10;
+    else return 0;
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -11,11 +21,33 @@ export const App: React.FC = () => {
           {[...Array(5)].map((_, index) => (
             <span
               style={{
-                fontSize: 36,
+                fontSize: 136,
+                cursor: "pointer",
+                // color: starCount <= index ? "white" : "yellow",
+                // color: "linear-gradient(to right, green 70%, white 30%)"
+                background: `linear-gradient(to right, yellow ${getYellowWidth(
+                  index
+                )}%, white 0%)`,
+                WebkitBackgroundClip: "text",
+                color: "transparent"
+              }}
+              // className="Star"
+              onClick={() => setStarCount(index + 1)}
+              key={index}
+            >
+              &#9733;
+            </span>
+          ))}
+        </div>
+        <div>
+          {[...Array(5)].map((_, index) => (
+            <span
+              style={{
+                fontSize: 136,
                 cursor: "pointer",
                 color: starCount <= index ? "white" : "yellow"
               }}
-              className="Star"
+              // className="Star"
               onClick={() => setStarCount(index + 1)}
               key={index}
             >
